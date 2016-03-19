69d628d31d90c7f90d053f82023d1b2912f05ca7
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2016-03-19 12:56:47 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ Bundler.require(*Rails.groups)
 
 module RedmineApp
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
+    config.encoding = 'utf-8'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
