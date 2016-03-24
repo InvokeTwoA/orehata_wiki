8130595c465db415f5eac4629a93e7ec03f39bed
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2016-03-24 19:00:24 +0900

    fix

--- app/helpers/wiki_helper.rb
+++ app/helpers/wiki_helper.rb
@@ -1,22 +1,3 @@
-# encoding: utf-8
-#
-# Redmine - project management software
-# Copyright (C) 2006-2016  Jean-Philippe Lang
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 module WikiHelper
   include Redmine::Export::PDF::WikiPdfHelper
 
@@ -64,4 +45,19 @@ module WikiHelper
       project_wiki_page_path(page.project, page.title)
     end
   end
+
+  def sort_activity_events_for_right_bar(events)
+    events_by_group = events.group_by(&:event_group)
+    sorted_events = []
+    events.sort {|x, y| y.event_datetime <=> x.event_datetime}.each do |event|
+      if group_events = events_by_group.delete(event.event_group)
+        group_events.sort {|x, y| y.event_datetime <=> x.event_datetime}.each_with_index do |e, i|
+          sorted_events << [e, i > 0]
+        end
+      end
+    end
+    sorted_events
+  end
+
+
 end
