3f3e943b8e610df4d931bf8c50414e198afbc34a
ItoNaoya <itonaoya@ItoNaoya-no-MacBook-Pro.local>
2016-04-13 18:06:54 +0900

    fix comment

--- app/models/wiki_comment.rb
+++ app/models/wiki_comment.rb
@@ -4,6 +4,4 @@ class WikiComment < ActiveRecord::Base
   scope :active, -> { where(active_flag: true) }
   scope :inactive, -> { where("active_flag IS NULL OR active_flag = false") }
 
-  # kaminari
-  # page_method_name :comment_page
 end
