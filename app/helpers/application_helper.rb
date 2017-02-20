414238931d96dc298a937e6bcaae85be75493103
ItoNaoya <itonaoya@ItoNaoya-no-MacBook-Pro.local>
2017-02-21 02:30:12 +0900

    魔女と百鬼兵２を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1337,6 +1337,7 @@ module ApplicationHelper
     return link_to "マルディタカスティーラ -ドン・ラミロと呪われた大地ex-　徹底攻略wiki", root_project_wiki_page_index_path(project_id: params[:id]) if params[:id] == 'castilla'
     return link_to "カルドセプトリボルト　徹底攻略wiki",     root_project_wiki_page_index_path(project_id: params[:project_id]) if controller_name == 'culdcepts'
     return link_to "カルドセプトリボルト　徹底攻略wiki",     root_project_wiki_page_index_path(project_id: params[:id]) if params[:id] == 'culdcept'
+    return link_to "魔女と百鬼兵２　徹底攻略wiki",           root_project_wiki_page_index_path(project_id: params[:id]) if params[:id] == 'majo100_2'
     return link_to "ルフランの地下迷宮と魔女ノ旅団",         root_project_wiki_page_index_path(project_id: params[:id]) if params[:id] == 'refrain'
     return link_to "ヒーラーは二度死ぬ　徹底攻略wiki",       root_project_wiki_page_index_path(project_id: params[:id]) if params[:id] == 'healer'
     return link_to "クラシックダンジョン戦国　徹底攻略wiki", root_project_wiki_page_index_path(project_id: params[:id]) if params[:id] == 'cl_dungeon_sengoku'
@@ -1356,6 +1357,8 @@ module ApplicationHelper
       link_to "クラシックダンジョン戦国　徹底攻略wiki", root_project_wiki_page_index_path(project_id: params[:project_id])
     when 'culdcept' then
       link_to "カルドセプトリボルト　徹底攻略wiki", root_project_wiki_page_index_path(project_id: params[:project_id])
+    when 'majo100_2' then
+      link_to "魔女と百鬼兵２　徹底攻略wiki", root_project_wiki_page_index_path(project_id: params[:project_id])
     when 'healer' then
       link_to "ヒーラーは二度死ぬ　徹底攻略wiki", root_project_wiki_page_index_path(project_id: params[:project_id])
     when 'refrain' then
@@ -1394,6 +1397,8 @@ module ApplicationHelper
       '「クラシックダンジョン戦国」の攻略wikiです。効率の良い稼ぎ方や、ダンジョンを制覇するためのパーティー構成の考察などを行ってます。'
     when 'culdcept' then
       '「カルドセプトリボルト」の攻略wikiです。カード情報や対戦の考察を行ってます。'
+    when 'majo100_2' then
+      '「魔女と百鬼兵２」の攻略wikiです。エンディング分岐の考察なども行ってます。'
     when 'orehata_tori' then
       '俺に働けって言われても酉の攻略wikiです。効率の良い稼ぎ方や、ダンジョンを制覇するためのパーティー構成の考察などを行ってます。'
     when 'healer' then
@@ -1437,6 +1442,8 @@ module ApplicationHelper
       ['クラシックダンジョン', 'クラダン', '戦国', 'クラシックダンジョン戦国', '攻略', 'wiki', '考察', '稼ぎ', 'まとめ', '考察', '徹底', '徹底攻略']
     when 'culdcept' then
       ['カルドセプト', 'リボルト', 'リボルド', '対戦', '攻略', 'wiki', '考察', '稼ぎ', 'まとめ', 'ブック', 'カード', '徹底', '徹底攻略', 'テクニック', 'コンボ']
+    when 'majo100_2' then
+      ['魔女と百鬼兵２', '魔女百', '続編', '魔女百２', '攻略', 'wiki', '考察', '稼ぎ', 'まとめ', 'エンディング', '分岐', '徹底', '徹底攻略']
     when 'orehata_tori' then
       ['俺に働けって言われても', '酉', '攻略', 'wiki', '俺働', '稼ぎ', '俺働酉', '俺に働けと言われても', 'おれはた', '俺に働けって言われても酉', 'まとめ', '考察', '徹底', '徹底攻略']
     when 'healer' then
@@ -1473,6 +1480,8 @@ module ApplicationHelper
       'クラシックダンジョン戦国　徹底攻略wiki'
     when 'culdcept' then
       'カルドセプトリボルト　徹底攻略wiki'
+    when 'majo100_2' then
+      '魔女と百鬼兵２　徹底攻略wiki'
     when 'orehata_tori' then
       '俺に働けって言われても酉　徹底攻略wiki'
     when 'healer' then
