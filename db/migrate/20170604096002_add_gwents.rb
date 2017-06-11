dbf5050875f54d88d283c7a9414676cd016251ee
ItoNaoya <itonaoya@ItoNaoya-no-MacBook-Pro.local>
2017-06-11 13:06:23 +0900

    fix typo

--- db/migrate/20170604096002_add_gwents.rb
+++ db/migrate/20170604096002_add_gwents.rb
@@ -1,4 +1,4 @@
-class CreateGwents < ActiveRecord::Migration
+class AddGwents < ActiveRecord::Migration
   def change
     add_column :gwents, :is_dwarf, :boolean
     add_column :gwents, :skill_command, :boolean
